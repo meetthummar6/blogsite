0f9b3224096895cf8ac6a164b4e02924383eb0e4
Meet Thummar <meetthummar132@gmail.com>
2023-05-18 21:44:12 +0530

    Category added in navbar

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post,Category
@@ -13,10 +14,21 @@ class HomeView(ListView):
     # ordering=['-post_date']
     ordering=['-id']
 
+    def get_context_data(self,*args, **kwargs):
+        cat_menu= Category.objects.all()
+        context=super(HomeView,self).get_context_data(*args,**kwargs)
+        context['cat_menu']=cat_menu
+        return context
+
 class ArticleDetailView(DetailView):
     model=Post
     template_name='article_details.html'
 
+    def get_context_data(self,*args, **kwargs):
+        cat_menu= Category.objects.all()
+        context=super(ArticleDetailView,self).get_context_data(*args,**kwargs)
+        context['cat_menu']=cat_menu
+        return context
 class AddPostView(CreateView):
     model=Post
     form_class=PostForm
@@ -43,3 +55,7 @@ class AddCategoryView(CreateView):
 def CategoryView(request,cats):
     category_posts=Post.objects.filter(category=cats.replace('-',' '))
     return render(request,'category.html',{'cats':cats.replace('-',' '),'cat_post':category_posts})
+
+def CategoryListView(request):
+    cat_menu_list=Category.objects.all()
+    return render(request,'category_list.html',{'cat_menu_list':cat_menu_list})
